7eed9e479c0692c04a1b0d25c9610c040e7dd1cf
Mostafa Asadi <mostafaasadi73@gmail.com>
2024-04-03 20:32:08 +0330

    add integrator version

---
+++ bi_version.py
@@ -0,0 +1,28 @@
+VSVersionInfo(
+  ffi=FixedFileInfo(
+    filevers=(1, 0, 0, 0),
+    prodvers=(1, 0, 0, 0),
+    mask=0x3f,
+    flags=0x0,
+    OS=0x40004,
+    fileType=0x1,
+    subtype=0x0,
+    date=(0, 0)
+    ),
+  kids=[
+    StringFileInfo(
+      [
+      StringTable(
+        u'040904B0',
+        [StringStruct(u'CompanyName', u'Persepolis Team'),
+        StringStruct(u'FileDescription', u'Persepolis Integrator'),
+        StringStruct(u'FileVersion', u'1.0.0.0'),
+        StringStruct(u'InternalName', u'Persepolis Integrator'),
+        StringStruct(u'LegalCopyright', u'\xa9 GNU General Public License v3.0'),
+        StringStruct(u'OriginalFilename', u'PersepolisBI.exe'),
+        StringStruct(u'ProductName', u'Persepolis Integrator'),
+        StringStruct(u'ProductVersion', u'1.0.0.0')])
+      ]),
+    VarFileInfo([VarStruct(u'Translation', [1033, 1200])])
+  ]
+)
