c4e1fefde2225fc1304cd72931b987e097690c9e
Mostafa Asadi <mostafaasadi73@gmail.com>
2025-04-19 23:33:07 +0330

    update to v 5.1.0

--- bi_version.py
+++ bi_version.py
@@ -1,7 +1,7 @@
 VSVersionInfo(
   ffi=FixedFileInfo(
-    filevers=(5, 0, 2, 0),
-    prodvers=(5, 0, 2, 0),
+    filevers=(5, 1, 0, 0),
+    prodvers=(5, 1, 0, 0),
     mask=0x3f,
     flags=0x0,
     OS=0x40004,
@@ -16,12 +16,12 @@ VSVersionInfo(
         u'040904B0',
         [StringStruct(u'CompanyName', u'Persepolis Team'),
         StringStruct(u'FileDescription', u'Persepolis Integrator'),
-        StringStruct(u'FileVersion', u'5.0.2.0'),
+        StringStruct(u'FileVersion', u'5.1.0.0'),
         StringStruct(u'InternalName', u'Persepolis Integrator'),
         StringStruct(u'LegalCopyright', u'\xa9 GNU General Public License v3.0'),
         StringStruct(u'OriginalFilename', u'PersepolisBI.exe'),
         StringStruct(u'ProductName', u'Persepolis Integrator'),
-        StringStruct(u'ProductVersion', u'5.0.2.0')])
+        StringStruct(u'ProductVersion', u'5.1.0.0')])
       ]),
     VarFileInfo([VarStruct(u'Translation', [1033, 1200])])
   ]
