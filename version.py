aa9ab7e8ac4ab0b4f58724f06660cf56b4398c4c
Mostafa Asadi <mostafaasadi73@gmail.com>
2025-05-28 12:30:42 +0330

    5.1.1.0

--- version.py
+++ version.py
@@ -1,7 +1,7 @@
 VSVersionInfo(
   ffi=FixedFileInfo(
-    filevers=(5, 1, 0, 0),
-    prodvers=(5, 1, 0, 0),
+    filevers=(5, 1, 1, 0),
+    prodvers=(5, 1, 1, 0),
     mask=0x3f,
     flags=0x0,
     OS=0x40004,
@@ -16,12 +16,12 @@ VSVersionInfo(
         u'040904B0',
         [StringStruct(u'CompanyName', u'Persepolis Team'),
         StringStruct(u'FileDescription', u'Persepolis Download Manager'),
-        StringStruct(u'FileVersion', u'5.1.0.0'),
+        StringStruct(u'FileVersion', u'5.1.1.0'),
         StringStruct(u'InternalName', u'Persepolis Download Manager'),
         StringStruct(u'LegalCopyright', u'\xa9 GNU General Public License v3.0'),
         StringStruct(u'OriginalFilename', u'Persepolis Download Manager.exe'),
         StringStruct(u'ProductName', u'Persepolis Download Manager'),
-        StringStruct(u'ProductVersion', u'5.1.0.0')])
+        StringStruct(u'ProductVersion', u'5.1.1.0')])
       ]),
     VarFileInfo([VarStruct(u'Translation', [1033, 1200])])
   ]
